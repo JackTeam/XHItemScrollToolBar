3a1a78cacea98d478bc111a63f072b89fdec699c
Jack <xhzengAIB@gmail.com>
2014-01-23 16:11:45 +0000

    implementation XHItemScrollToolBar logic

--- Source/XHItemScrollToolBar.m
+++ Source/XHItemScrollToolBar.m
@@ -16,11 +16,68 @@
 
 @implementation XHItemScrollToolBar
 
+- (NSArray *)itemViews {
+    if (!_itemViews) {
+        _itemViews = [[NSArray alloc] init];
+    }
+    return _itemViews;
+}
+
+- (void)setItems:(NSArray *)items {
+    if (!items)
+        return;
+    _items = items;
+}
+
+- (void)reloadData {
+    [self _layoutSubviews];
+}
+
+- (void)_layoutSubviews {
+    NSMutableArray *itemVies = [[NSMutableArray alloc] initWithCapacity:5];
+    for (XHItem *item in self.items) {
+        NSInteger index = [self.items indexOfObject:item];
+        item.index = index;
+        CGRect bottomItemFrame = CGRectMake(index * (self.itemWidth + self.itemPaddingX) + self.itemPaddingX, self.itemPaddingY, self.itemWidth, CGRectGetHeight(self.bounds) - self.itemPaddingY * 2);
+        XHItemView *itemView = [[XHItemView alloc] initWithFrame:bottomItemFrame item:item];
+        if (index == self.selectIndex) {
+            itemView.selected = YES;
+        }
+        [itemView addTarget:self action:@selector(itemViewClicked:) forControlEvents:UIControlEventTouchUpInside];
+        itemView.frame = bottomItemFrame;
+        [self addSubview:itemView];
+        [itemVies addObject:itemView];
+    }
+    self.itemViews = itemVies;
+    
+    [self _setupContentSize];
+}
+
+- (void)_setupContentSize {
+    self.contentSize = CGSizeMake((self.itemWidth + self.itemPaddingX) * self.items.count + self.itemPaddingX, 0);
+}
+
+- (void)itemViewClicked:(XHItemView *)itemView {
+    for (XHItemView *_itemView in self.itemViews) {
+        if (itemView == _itemView) {
+            _itemView.selected = YES;
+        } else {
+            _itemView.selected = NO;
+        }
+    }
+    if (itemView.item.itemSelectedCompled) {
+        itemView.item.itemSelectedCompled(itemView);
+    }
+}
+
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        self.showsHorizontalScrollIndicator = NO;
+        self.showsVerticalScrollIndicator = NO;
     }
     return self;
 }
