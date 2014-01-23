9fa1b1d4fcfdde5a4f3d93b1570c4117ffb5b596
Jack <xhzengAIB@gmail.com>
2014-01-23 16:04:32 +0000

    Add file with XHItem class

---
+++ Source/XHItem.m
@@ -0,0 +1,13 @@
+//
+//  XHItem.m
+//  XHItemScrollToolBar
+//
+//  Created by 曾 宪华 on 14-1-23.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHItem.h"
+
+@implementation XHItem
+
+@end
