ef5cdabf8ea9e361b4bdf124af7de210ddd9c957
Jack <xhzengAIB@gmail.com>
2014-01-23 15:57:47 +0000

    Firstly init project

---
+++ XHItemScrollToolBar/XHItemScrollToolBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XHItemScrollToolBar
+//
+//  Created by 曾 宪华 on 14-1-23.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ XHItemScrollToolBar/XHItemScrollToolBarTests/XHItemScrollToolBarTests.m
@@ -0,0 +1,34 @@
+//
+//  XHItemScrollToolBarTests.m
+//  XHItemScrollToolBarTests
+//
+//  Created by 曾 宪华 on 14-1-23.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XHItemScrollToolBarTests : XCTestCase
+
+@end
+
+@implementation XHItemScrollToolBarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
